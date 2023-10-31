ca884aea57953f78bd4b6ca5e9d275bb9caaa245
Sarah Goodell <sarahlgoodell1@gmail.com>
2023-10-31 11:00:59 -0400

    Added comments to organize which routes I need to make

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,18 @@
+// The api/users/ endpoint
+
+// GET all users
+
+// GET a single user by its _id and populated thought and friend data
+
+// POST a new user
+
+// PUT to update a user by its _id
+
+// DELETE to remove a user by its _id
+
+
+// /api/users/:userId/friends/:friendId
+
+// POST to add a new friend to a user's friend list
+
+// DELETE to remove a friend from a user's friend list
